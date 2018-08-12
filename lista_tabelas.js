7163a9f42cdc2bd61b21105f2c12e8345e0e29ea
Jose <32643363+jl-guerreiro@users.noreply.github.com>
2018-08-12 17:23:09 -0300

    first commit

---
+++ lista_tabelas.js
@@ -0,0 +1,23 @@
+function getTabelas() {
+  return {
+    "lista": {
+      "tabela": [{
+          "id": "Tabela 1",
+          "numbers": "1,2,3,4,5,6"
+        },
+        {
+          "id": "Tabela 2",
+          "numbers": "1,2,3,4,5,6,7,8"
+        },
+	{
+          "id": "Tabela 3",
+          "numbers": "10,20,30,44,55,60"
+        },
+	{
+          "id": "Tabela 4",
+          "numbers": "50,49,33,41,45,16,7,58"
+        }
+      ]
+    }
+  };
+}
